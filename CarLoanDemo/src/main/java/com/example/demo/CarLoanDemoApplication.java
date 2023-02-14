7e1336c4ae84a8d3946b9e8457e880b570f9b2c3
roshanpatil110598 <patilroshan503@gmail.com>
2023-02-14 12:35:41 +0530

    project setup

---
+++ CarLoanDemo/src/main/java/com/example/demo/CarLoanDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarLoanDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarLoanDemoApplication.class, args);
+	}
+
+}
